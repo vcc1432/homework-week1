fc19f43c35997edda7cb224a855c6d53a16e34b4
vcc1432 <valerie.gersen@gmail.com>
2018-08-11 09:54:57 +0200

    Created hero object, definedd (empty) game logic functions and rest function

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,24 @@
+// Write your JS here
+let hero = {
+    name: "Altair",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "blade",
+        damage: 2
+    }
+}
+
+function rest(hero) {
+    hero.health = 10
+    return hero
+}
+
+function pickUpItem() {
+
+}
+
+function equipWeapon() {
+
+}
