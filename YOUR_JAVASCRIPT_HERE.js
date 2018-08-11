5287621703f44a3137cda3927673f87549f4a197
vcc1432 <valerie.gersen@gmail.com>
2018-08-11 15:18:16 +0200

    Created function displayStats, created form to change name, called displayStats to update page

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -10,20 +10,17 @@ let hero = {
     }
 }
 
-console.log(hero)
 
 function rest(object) {
     object.health = 10
-    console.log(hero)
-    return object
-
     
+    displayStats()
 }
 
 function pickUpItem(character, object) {
     character.inventory.push(object) 
 
-    console.log(hero)
+    displayStats()
 }
 
 let dagger = {
@@ -31,13 +28,38 @@ let dagger = {
     damage: 2
 }
 
-function equipWeapon(character) {
-        
+function equipWeapon(character) {      
     if (character.inventory.length <= 0) {
         return;
     } else {
         character.weapon = character.inventory[0]
     }
 
-console.log(hero)
+    displayStats()
+}
+
+function displayStats() {   
+    let heroStats = document.getElementById("stats")
+    heroStats.innerHTML = 
+    `Name: ${hero.name}<br>
+    Health: ${hero.health}<br>
+    Weapontype: ${hero.weapon.type}<br>
+    Weapondamage: ${hero.weapon.damage}`
+}
+
+function submitName(event) {
+    event.preventDefault()
+    var inputField = document.getElementById("newName")
+    var chosenName = inputField.value
+
+    createName(chosenName)
+    
+    inputField.value = null;
+
+    function createName(name) {
+        hero.name = name
+    }
+
+    displayStats()
+
 }
